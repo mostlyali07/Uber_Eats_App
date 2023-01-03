e44229c6b63968ffed8525479e82569e05e07faa
mostlyali07 <104094853+mostlyali07@users.noreply.github.com>
2023-01-03 23:50:48 +0500

    Update

--- Screens/Home.js
+++ Screens/Home.js
@@ -1,11 +1,15 @@
-import { View, Text, SafeAreaView } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView } from "react-native";
+import React from "react";
 import HeaderTabs from "../Components/Headertabs";
+import SearchBar from "../Components/SearchBar";
 
 export default function Home() {
   return (
-    <SafeAreaView>
-      <HeaderTabs />
+    <SafeAreaView style={{ backgroundColor:'#eee', flex: 1}}>
+      <View style={{ backgroundColor:'#fff', padding: 15}}>
+        <HeaderTabs />
+        <SearchBar/>
+      </View>
     </SafeAreaView>
-  )
+  );
 }
