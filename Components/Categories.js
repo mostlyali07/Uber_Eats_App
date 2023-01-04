1fd550cd84de18c358fd33468a03b15d0adb8d42
mostlyali07 <104094853+mostlyali07@users.noreply.github.com>
2023-01-04 18:11:25 +0500

    Update

--- Components/Categories.js
+++ Components/Categories.js
@@ -1,15 +1,60 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import { View, Text, Image, ScrollView } from "react-native";
+import React from "react";
 
-const items = [{
+const items = [
+  {
     image: require("../Assests/images/shopping-bag.png"),
     text: "Pick-up",
-}]
+  },
+  {
+    image: require("../Assests/images/soft-drink.png"),
+    text: "Soft Drinks",
+  },
+  {
+    image: require("../Assests/images/bread.png"),
+    text: "Bakery Items",
+  },
+  {
+    image: require("../Assests/images/fast-food.png"),
+    text: "Fast Foods",
+  },
+  {
+    image: require("../Assests/images/deals.png"),
+    text: "Deals",
+  },
+  {
+    image: require("../Assests/images/coffee.png"),
+    text: "Coffee & Tea",
+  },
+  {
+    image: require("../Assests/images/desserts.png"),
+    text: "Desserts",
+  },
+];
 
 export default function Categories() {
   return (
-    <View>
-      <Text>Categories</Text>
+    <View style={{
+        marginTop: 5,
+        backgroundColor: "#fff",
+        paddingVertical: 10,
+        paddingLeft: 20,
+    }}>
+      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+        {items.map((item, index) => (
+          <View key={index} style={{ alignItems: "center", marginRight: 30 }}>
+            <Image
+              source={item.image}
+              style={{
+                width: 50,
+                height: 40,
+                resizeMode: "contain",
+              }}
+            />
+            <Text style={{ fontSize: 13, fontWeight: "900" }}>{item.text}</Text>
+          </View>
+        ))}
+      </ScrollView>
     </View>
-  )
+  );
 }
--- Screens/Home.js
+++ Screens/Home.js
@@ -1,4 +1,4 @@
-import { View, Text, SafeAreaView } from "react-native";
+import { View, Text, SafeAreaView, ScrollView } from "react-native";
 import React from "react";
 import HeaderTabs from "../Components/Headertabs";
 import SearchBar from "../Components/SearchBar";
@@ -6,12 +6,14 @@ import Categories from "../Components/Categories";
 
 export default function Home() {
   return (
-    <SafeAreaView style={{ backgroundColor:'#eee', flex: 1}}>
-      <View style={{ backgroundColor:'#fff', padding: 15}}>
+    <SafeAreaView style={{ backgroundColor: "#eee", flex: 1 }}>
+      <View style={{ backgroundColor: "#fff", padding: 15 }}>
         <HeaderTabs />
-        <SearchBar/>
+        <SearchBar />
       </View>
-      <Categories />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <Categories />
+      </ScrollView>
     </SafeAreaView>
   );
 }
