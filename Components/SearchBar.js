ee68eb76a8987ab07c86795bf40ea33099283f56
mostlyali07 <104094853+mostlyali07@users.noreply.github.com>
2023-01-18 17:16:06 +0500

    Update

--- Components/SearchBar.js
+++ Components/SearchBar.js
@@ -4,11 +4,16 @@ import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplet
 import Ionicons from "react-native-vector-icons/Ionicons";
 import AntDesign from "react-native-vector-icons/AntDesign";
 
-export default function SearchBar() {
+export default function SearchBar({cityHandler}) {
   return (
     <View style={{ marginTop: 15, flexDirection: "row" }}>
       <GooglePlacesAutocomplete
-        query={{ key: "AIzaSyDZQReF6D9_KCD5RnjdP5VVRlgYjVVlgec" }}
+        query={{ key: "AIzaSyBysami_Meew_8O4aCEjMwB83TNGGMweKc" }}
+        onPress={(data, details = null) => {
+          console.log(data.description);
+          const city = data.description.split(",")[0];
+          cityHandler(city);
+        }}
         placeholder="Search..."
         styles={{
           textInput: {
--- Screens/Home.js
+++ Screens/Home.js
@@ -12,9 +12,10 @@ const YELP_API_KEY =
 
 export default function Home() {
   const [restaurantData, setRestaurantData] = React.useState(localRestaurant);
+  const [city, setCity] = useState("San Francisco");
   const getRestaurantsFromYelp = () => {
     const yelpUrl =
-      "https://api.yelp.com/v3/businesses/search?term=restaurants&location=SanFrancisco";
+      `https://api.yelp.com/v3/businesses/search?term=restaurants&location=${city}`;
     const apiOptions = {
       headers: {
         Authorization: `Bearer ${YELP_API_KEY}`,
@@ -26,16 +27,16 @@ export default function Home() {
   };
   useEffect(() => {
     getRestaurantsFromYelp();
-  }, []);
+  }, [city]);
   return (
     <SafeAreaView style={{ backgroundColor: "#eee", flex: 1 }}>
       <View style={{ backgroundColor: "#fff", padding: 15 }}>
         <HeaderTabs />
-        <SearchBar />
+        <SearchBar cityHandler={setCity} />
       </View>
       <ScrollView showsVerticalScrollIndicator={false}>
         <Categories />
-        <RestaurantItems restaurantData={restaurantData} />
+        <RestaurantItems restaurantData={restaurantData}/>
       </ScrollView>
     </SafeAreaView>
   );
