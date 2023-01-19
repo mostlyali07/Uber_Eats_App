805ddde70533b38f7de559a737d2483518f41b1c
mostlyali07 <104094853+mostlyali07@users.noreply.github.com>
2023-01-19 23:20:13 +0500

    Update

--- App.js
+++ App.js
@@ -1,18 +1,16 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+// import { StyleSheet } from "react-native";
 import Home from "./Screens/Home";
 
 export default function App() {
-  return (
-    <Home />
-  );
+  return <Home />;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+// });
